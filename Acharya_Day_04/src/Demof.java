f7f68940a89b6151ea71984bbad06376a3151b3a
krish <krish@LAPTOP-SA99OA8V>
2022-06-03 21:01:41 +0530

    first commit

---
+++ Acharya_Day_04/src/Demof.java
@@ -0,0 +1,17 @@
+
+public class Demof {
+	float x;
+
+	public static void main(String[] args) {
+		Demof myobj1 = new Demof();
+	Demof myobj2 = new Demof();
+	myobj1.x = 3.4f;
+	myobj2.x = 4.5f;
+	System.out.println(myobj1.x);
+	System.out.println(myobj2.x);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
