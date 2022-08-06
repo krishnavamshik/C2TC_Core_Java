395968c40dff51b60c5bf7783694773a9ab40ec0
krish <krish@LAPTOP-SA99OA8V>
2022-08-06 22:47:15 +0530

    first commit

---
+++ Acharya_Day_19/src/com/acharya/junit5/Demo.java
@@ -0,0 +1,16 @@
+package com.acharya.junit5;
+
+import org.junit.jupiter.api.Test;
+
+public class Demo {
+	@Test
+	 void Display() {
+		 System.out.println("Hello");
+	 }
+
+	public static void main(String[] args) {
+		 Demo obj = new Demo();
+			obj.Display();
+	}
+
+}
